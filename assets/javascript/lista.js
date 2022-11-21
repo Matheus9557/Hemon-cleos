151a2a9131a1f3dfd9cd5c4a2507294712599604
Matheus Gomes <79982182+Matheus9557@users.noreply.github.com>
2022-11-21 20:59:39 -0300

    Add files via upload

--- assets/javascript/lista.js
+++ assets/javascript/lista.js
@@ -1,18 +1,44 @@
-function listarPontos() {
-    fetch("http://localhost:3000/pontos", {
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 2,
+    center: new google.maps.LatLng(2.8, -187.3),
+    mapTypeId: "terrain",
+  });
+
+  function listarPontos() {
+    fetch("http://localhost:3000/router/lista", {
       method: "GET",
     })
       .then((response) => response.json())
       .then((data) => {
         lista = document.getElementById("pontos");
-        // Considerando que data é um array de objetos
-        // Ao final desse map, temos uma lista de elementos
+
         pontosRecarregados = data.map((ponto) => {
-          const ponto = document.createElement("pre");
+          const ponto = document.createElement("Ponto");
           ponto.textContent = JSON.stringify(ponto);
           return ponto;
         });
-        // E vamos "substituir" o conteúdo da lista
         lista.replaceChildren(...pontosRecarregados);
       });
+  };
+};
+
+const Pontos = function (ponto) {
+  for (let i = 0; i < ponto.features.length; i++) {
+    const coords = ponto.features[i].geometry.coordinates;
+    const latLng = new google.maps.LatLng(coords[1], coords[0]);
+
+    new google.maps.Marker({
+      position: latLng,
+      map: map,
+    });
   }
+};
+
+window.initMap = initMap;
+window.Pontos = Pontos;
+
+
+
